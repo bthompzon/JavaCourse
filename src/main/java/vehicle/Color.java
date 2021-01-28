a0e4349e236e4946d0e51079449b7f76403f3081
Charles Bradley Thompson <charlesbradleythompson@Charless-MacBook-Pro.local>
2021-01-27 19:48:56 -0800

    Module 2 homework yo

--- src/main/java/vehicle/Color.java
+++ src/main/java/vehicle/Color.java
@@ -1,19 +1,25 @@
 package vehicle;
 
 public enum Color {
-    SLOW_SILVER("Slow Silver"),
-    MELLO_YELLOW("Mello Yell Oh"),
-    RACING_RED("Racing Red"),
-    GOLDEN_GREEN("Golden Green");
+    SLOW_SILVER("Slow Silver", "#C0C0C0"),
+    MELLO_YELLOW("Mello Yell Oh", "#FFFF00"),
+    RACING_RED("Racing Red", "#FF0000"),
+    GOLDEN_GREEN("Golden Green", "#7CFC00");
 
     private String displayValue;
+    private String paintCode;
 
-    private Color(String displayValue) {
+    private Color(String displayValue, String paintCode) {
         this.displayValue = displayValue;
+        this.paintCode = paintCode;
     }
 
     @Override
     public String toString() {
         return displayValue;
+    }
+
+    public String getPaintCode() {
+        return paintCode;
     }
 }
